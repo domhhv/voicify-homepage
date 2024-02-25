6d0066de4bfc740c787c4a39ebf065569907ce39
Dominik Hryshaiev <domhryshaiev@gmail.com>
2024-02-25 15:55:25 +0200

    feat(category): add voice category chips (#8)
    * feat(category): add voice category chips
    
    * Use clsx utility
    
    * Remove commented code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { AppBanner } from '../components';
+import { AppBanner, VoiceCategoryChipsList } from '../components';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-3">
+    <main className="flex min-h-screen flex-col items-center px-3">
       <AppBanner />
+      <VoiceCategoryChipsList />
     </main>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './app-header';
 export * from './app-banner';
 export * from './app-footer';
 export * from './icons';
+export * from './voice-category-chips';
